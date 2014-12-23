7f3bf1678e504efc3f1c922ed88136937d613d5e
Rosie Ye <rosieye1992@gmail.com>
2014-12-23 16:18:44 +0800

    service

--- SshPortForward.sh
+++ SshPortForward.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -L 8000:localhost:8000 lab.mian.moe
+ssh -Nf -L 8000:localhost:8000 lab.mian.moe
--- hserver.py
+++ hserver.py
@@ -4,6 +4,8 @@
 from flask import Flask, request
 import sys
 import readvec
+import numpy as np
+import scipy.spatial.distance.cosine
 
 reload(sys)
 sys.setdefaultencoding('UTF-8')
@@ -19,7 +21,10 @@ def wordvector():
     word = request.args.get('w', None)
     if word is None:
         return "BAD", 400
-    return word
+    vec = lex.get(word, None)
+    if not word:
+        return "BAD", 400
+    return vec
 
 @app.route("/wordsimi")
 def wordsimi():
@@ -27,11 +32,15 @@ def wordsimi():
     word2 = request.args.get('w2', None)
     if not word1 and not word2:
         return "BAD", 400
+    vec1 = lex.get(word1, None)
+    vec2 = lex.get(word2, None)
+    if not vec1 or not vec2:
+        return "BAD", 400
 #    return u"{}:{}".format(word1, word2)
-    return word1+word2
+    return cosine(vec1, vec2)
 
 
-
+lex = readvec('vec2.bin')
 app.run(debug=True, host='0.0.0.0', port=8000)
 
 
